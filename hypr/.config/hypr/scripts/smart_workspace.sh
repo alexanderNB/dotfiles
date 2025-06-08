7536879742f2a2208bbc818a2f580c3b815e641c
alexanderNB <alexander@bjerking.dk>
2025-06-08 14:01:32 +0200

    Commit from laptop with nvim

--- hypr/.config/hypr/scripts/smart_workspace.sh
+++ hypr/.config/hypr/scripts/smart_workspace.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+pc="laptop"
+
 command="$1"
 workspace="$2"
 
 cursorpos="$(hyprctl cursorpos)"
 cursorpos="${cursorpos%%,*}"
-
-if ((cursorpos < 1920)); then
-  workspace="$((workspace + 10))"
+if [[ $pc == "desktop" ]]; then
+    if ((cursorpos < 1920)); then
+        workspace="$((workspace + 10))"
+    fi
 fi
 hyprctl dispatch "$command" "$workspace"

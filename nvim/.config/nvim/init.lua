9cc81f6e27655d7c313d3bab8c7e05b32e5164c7
alexanderNB <alexander@bjerking.dk>
2025-06-22 23:36:24 +0200

    Telescope improvements

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 
 require("config.lazy")
+require("plugins.plugin_options.telescope")
+require("plugins.plugin_options.snacks")
 
 vim.cmd("colorscheme arctic")
 
